4f136500892b7d3c27620f3aa0dfc677806d21d2
Rouela D <81519103+angelorenz70@users.noreply.github.com>
2022-11-01 18:05:31 +0800

    done first tutorial

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,59 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+/*
+Route::get();
+Route::post();
+Route::put();
+Route::patch();
+Route::delete();
+Route::option();
+*/
+
+//Route::match(['get', 'post'],'/', function(){
+//    return 'POST and GET is allowed';
+//});
+
+//Route::view('/web', 'welcome');
+
 Route::get('/', function () {
-    return view('welcome');
+    return 'welcome';
+});
+
+//Route::redirect('/home', '/website');
+
+/*Route::get('/users', function(Request $request){
+    dd($request);
+    return 'requested';
+}); */
+
+
+/*Route::get('/get-text', function(){       //response
+    return response('Hello...', 200)
+        ->header('content-Type', 'text/plain');
+});*/
+
+/* 
+Route::get('/user/{id}', function($id){     //if 1 parameter
+    return response($id, 200);
+});*/
+
+
+/*
+Route::get('/user/{id}/{name}', function($id, $name){     //if 2 or more parameter
+    return response($id.' | '.$name, 200);
+});*/
+
+
+Route::get('/request-json', function(){     //request to user json file
+    return response() ->json(['name' => 'angelorenz', 'age' => '20']);
+});
+
+Route::get('/request-download', function(){     //request to user to download
+    $path = public_path().'/sample.txt';
+    $name = 'sample.txt';
+    $headers = array(
+        'Content-Type : application/text-plain',
+    );
+    return response() ->download($path,$name,$headers);
 });
